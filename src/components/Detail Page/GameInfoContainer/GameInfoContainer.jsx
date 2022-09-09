d40f9fe983d286bb997f20743b59212bd8b4de08
becp12 <rebecca-preece@outlook.com>
2022-09-09 15:57:35 -0700

    Adding various styling (GameDetail Header NavBar)

--- src/components/Detail Page/GameInfoContainer/GameInfoContainer.jsx
+++ src/components/Detail Page/GameInfoContainer/GameInfoContainer.jsx
@@ -1,19 +1,23 @@
 import './GameInfoContainer.css';
 import Summary from '../Summary/Summary'
+import PlaySessionForm from '../PlaySessionForm/PlaySessionForm';
 
 export default function GameInfoContainer({ game }) {
     console.log(game)
     const platforms = game.platforms.map(p => <li>{p.name}</li>)
     return (
         <div className='game-info-container'>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="summary"><Summary game={game} /></div>
-                <div class="tab-pane fade" id="releaseDate">{game.releaseDate}</div>
-                <div class="tab-pane fade" id="platforms">
-                    <ul>
+            <div className="tab-content" id="myTabContent">
+                <div className="tab-pane fade show active" id="summary"><Summary game={game} /></div>
+                <div className="tab-pane fade" id="releaseDate">{game.releaseDate}</div>
+                <div className="tab-pane fade" id="platforms">
+                    <ul className="platform-list">
                         {platforms}
                     </ul>
                 </div>
+                <div className="tab-pane fade" id="playsessions">PLAY SESSION COMPONENT</div>
+                <div className="tab-pane fade" id="addplaysessions"><PlaySessionForm /></div>
+                
             </div>
         </div>
     );
