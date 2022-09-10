204eb645051e2b7d537fa83e67870798c61b145b
becp12 <rebecca-preece@outlook.com>
2022-09-10 16:59:44 -0700

    Adding view PlaySession Functionality

--- src/components/Detail Page/PlaySessions/PlaySessions.jsx
+++ src/components/Detail Page/PlaySessions/PlaySessions.jsx
@@ -6,59 +6,25 @@ import * as playsessionAPI from '../../../utilities/playsessions-api'
 export default function PlaySessions({ playSession, game, setPlaySession }) {
   const { gameId } = useParams();
 
-  useEffect(function () {
-    async function getSessions() {
-      const playSessions = await playsessionAPI.getAll(gameId);
-      setPlaySession(playSessions);
-    }
-    getSessions();
-  }, [gameId]);
-  // find game by id
-  // return all the play sessions
-  // map them as html elements so can place them in
-
-  
   return (
-      <div className="play-session-container">
-          <h3>Play Session Component</h3>
-          <div className="accordion" id="accordionExample">
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingOne">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
-                  Play Session 1 - maybe Date, Time and Duration appears here
-                </button>
-              </h2>
-              <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                  This is the first play session's notes (accordion body). It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
+    <div className="play-session-container">
+      <h3>Play Session Component</h3>
+      <div className="accordion" id="accordionExample">
+        {game.playSession.map(p => (
+          <div className="accordion-item">
+            <h2 className="accordion-header" id={`heading${p._id}`}>
+              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${p._id}`}>
+                {new Date(p.date).toLocaleDateString()}
+              </button>
+            </h2>
+            <div id={`collapse${p._id}`} className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                {p.notes}
               </div>
             </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingTwo">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">
-                  Play Session 2 - maybe Date, Time and Duration appears here
-                </button>
-              </h2>
-              <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                This is the second play session's notes (accordion body). It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingThree">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree">
-                  Play Session 3 - maybe Date, Time and Duration appears here
-                </button>
-              </h2>
-              <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                This is the third play session's notes (accordion body). It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-              </div>
-            </div>
-          </div>    
+          </div>
+        ))}
       </div>
+    </div>
   );
 }
